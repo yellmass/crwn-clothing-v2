62e6f64eda54538001bcfbd5b4d53445ede59c03
yellmass <ylmzbnc@users.noreply.github.com>
2023-08-29 22:27:09 -0400

    category item modified,so that directing to relative categories page when click

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import './category-item.styles.scss';
+import { useContext } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
+
+
 
 const CategoryItem = ({ category }) => {
+  const { categoriesMap } = useContext(CategoriesContext); 
   const { imageUrl, title } = category;
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+  categoriesMap && navigate( `shop/${Object.keys(categoriesMap).find((res)=>res===title)}`);
+  }
   return (
-    <div className='category-container'>
+    <div onClick={navigateHandler} className='category-container'>
       <div
         className='background-image'
         style={{
